2a67d6cc9538ffa815af00c1480a46b9266dc5a4
Kevin Cormier <kcormier@redhat.com>
2022-02-02 15:37:01 -0500

    Wait before installing plugin
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- rhacmstackem.sh
+++ rhacmstackem.sh
@@ -111,14 +111,14 @@ else
   ck disable-schedule $CLUSTERCLAIM_NAME
 fi
 
+echo "##### Waiting 10 minutes for ACM route to be created #####"
+sleep 600
+
 # Point to claimed cluster
 export KUBECONFIG=${LIFEGUARD_PATH}/clusterclaims/${CLUSTERCLAIM_NAME}/kubeconfig
 
 # Try to install dynamic plugin
 oc -n open-cluster-management patch $(oc -n open-cluster-management get helmreleases -o name | grep "console-chart") --type json --patch '[{"op": "add", "path": "/spec/plugin", "value": true}]' || true
-
-echo "##### Waiting 10 minutes for ACM route to be created #####"
-sleep 600
 
 # Send cluster information to Slack
 if [[ -n "${SLACK_URL}" ]] || ( [[ -n "${SLACK_TOKEN}" ]] && [[ -n "${SLACK_CHANNEL_ID}" ]] ); then
